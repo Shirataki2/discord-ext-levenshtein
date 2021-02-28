bd8ecbdfb7dc5ec526b9883cb15e88978a552c1c
Tomoya Ishii <shi2gx@gmail.com>
2021-02-28 13:15:04 +0900

    Fix blanks on docstring

--- discord/ext/levenshtein/__init__.py
+++ discord/ext/levenshtein/__init__.py
@@ -27,7 +27,7 @@ class Levenshtein:
             the Levenshtein distance is a string metric
             for measuring the difference between
             two sequences.
-            
+
             Informally, the Levenshtein distance
             between two words is the minimum number
             of single-character edits
